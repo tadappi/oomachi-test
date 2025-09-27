8d4ca0069d0ead10946ab145adf13747f852c0f9
tadappi <iskyimaging@gmail.com>
2025-09-27 16:04:00 +0900

    file test

--- data.js
+++ data.js
@@ -28,17 +28,71 @@ var APP_DATA = {
       ],
       "faceSize": 3600,
       "initialViewParameters": {
-        "yaw": 0.5312768623149751,
-        "pitch": 0.09904982294745501,
-        "fov": 0.40711721033155707
+        "yaw": 0.5195539502913142,
+        "pitch": 0.1655387776603412,
+        "fov": 0.396115032399867
       },
       "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": 2.7724847836889914,
-          "pitch": 0.30839483958675373,
+          "yaw": 0.5075745212803504,
+          "pitch": 0.19658999527474208,
+          "title": "水沢駅",
+          "text": "URLリンク"
+        },
+        {
+          "yaw": 0.9731265720551932,
+          "pitch": 0.28992063734284557,
+          "title": "水沢駅通り",
+          "text": "GE1"
+        },
+        {
+          "yaw": -2.3305712285602205,
+          "pitch": 0.6072966528700832,
+          "title": "乙女川公園",
+          "text": "photo1"
+        },
+        {
+          "yaw": 2.789895459978549,
+          "pitch": 0.3131089573392565,
           "title": "奥州市役所",
-"text": "<a href='https://www.city.oshu.iwate.jp/index.html' target='_blank'>奥州市役所サイトへ</a>"
+          "text": "URLリンク"
+        },
+        {
+          "yaw": -2.9339169444369677,
+          "pitch": 0.22363699510112234,
+          "title": "水沢病院",
+          "text": "URLリンク"
+        },
+        {
+          "yaw": 2.8599083233035287,
+          "pitch": 0.11211394672201891,
+          "title": "水沢小学校",
+          "text": "URL"
+        },
+        {
+          "yaw": -1.2951391384111375,
+          "pitch": 0.039542111257418355,
+          "title": "XYZ水沢",
+          "text": "photo2"
+        },
+        {
+          "yaw": 0.2140047348494747,
+          "pitch": 0.1518613714643049,
+          "title": "奥州病院",
+          "text": "URL"
+        },
+        {
+          "yaw": -0.8994644738945006,
+          "pitch": 0.07217685782761052,
+          "title": "コープあてるい",
+          "text": "photo3"
+        },
+        {
+          "yaw": -2.575206443597173,
+          "pitch": 0.08211396023305895,
+          "title": "水沢中学校",
+          "text": "URL"
         }
       ]
     }
@@ -46,7 +100,7 @@ var APP_DATA = {
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
